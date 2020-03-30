fbb31561098b19a7c7cb06011bb9453bb08bc868
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2020-03-30 12:28:36 +0200

    Parsing CBOR.Tag improved

--- Source/Utils/CBOR+String.swift
+++ Source/Utils/CBOR+String.swift
@@ -5,6 +5,9 @@
 */
 
 import SwiftCBOR
+#if canImport(Foundation)
+import Foundation
+#endif
 
 extension CBOR: CustomDebugStringConvertible {
     
@@ -13,6 +16,9 @@ extension CBOR: CustomDebugStringConvertible {
         case .unsignedInt(let value): return "\(value)"
         case .negativeInt(let value): return "\(value)"
         case .byteString(let bytes):
+            if bytes.isEmpty {
+                return "0 bytes"
+            }
             return "0x\(bytes.map { String(format: "%02X", $0) }.joined())"
         case .utf8String(let value): return "\"\(value)\""
         case .array(let array):
@@ -32,7 +38,7 @@ extension CBOR: CustomDebugStringConvertible {
                 }
                 .joined(separator: ", ") + "}"
         case .tagged(let tag, let cbor):
-            return "\(tag.rawValue): \(cbor)"
+            return "\(tag): \(cbor)"
         case .simple(let value): return "\(value)"
         case .boolean(let value): return "\(value)"
         case .null: return "null"
@@ -65,6 +71,51 @@ extension Dictionary where Key == CBOR, Value == CBOR {
             }
             .joined(separator: ", ")
             + "}"
+    }
+    
+}
+
+extension CBOR.Tag: CustomDebugStringConvertible {
+    
+    public var debugDescription: String {
+        switch rawValue {
+        case CBOR.Tag.standardDateTimeString.rawValue:
+            return "Standard Date Time String"
+        case CBOR.Tag.epochBasedDateTime.rawValue:
+            return "Epoch Based Date Time"
+        case CBOR.Tag.positiveBignum.rawValue:
+            return "Positive Big Number"
+        case CBOR.Tag.negativeBignum.rawValue:
+            return "Negative Big Number"
+        case CBOR.Tag.decimalFraction.rawValue:
+            return "Decimal Fraction"
+        case CBOR.Tag.bigfloat.rawValue:
+            return "Big Float"
+        case CBOR.Tag.expectedConversionToBase64URLEncoding.rawValue:
+            return "Expected Conversion oo Base64 URL Encoding"
+        case CBOR.Tag.expectedConversionToBase64Encoding.rawValue:
+            return "Expected Conversion to Base64 Encoding"
+        case CBOR.Tag.expectedConversionToBase16Encoding.rawValue:
+            return "Expected Conversion to Base16 Encoding"
+        case CBOR.Tag.encodedCBORDataItem.rawValue:
+            return "Encoded CBOR Data Item"
+        case CBOR.Tag.uri.rawValue:
+            return "URI"
+        case CBOR.Tag.base64Url.rawValue:
+            return "Base64 URL"
+        case CBOR.Tag.base64.rawValue:
+            return "Base64"
+        case CBOR.Tag.regularExpression.rawValue:
+            return "Regular Expression"
+        case CBOR.Tag.mimeMessage.rawValue:
+            return "MIME Message"
+        case CBOR.Tag.uuid.rawValue:
+            return "UUID"
+        case CBOR.Tag.selfDescribeCBOR.rawValue:
+            return "Self Describe CBOR"
+        default:
+            return "tag(\(rawValue))"
+        }
     }
     
 }
