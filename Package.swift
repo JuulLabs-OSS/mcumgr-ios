3e93cdc6eae7d1545ba1548a75ea07d35ce4ce2b
Florian Buerger <florian@florianbuerger.com>
2021-03-02 07:55:27 +0100

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "McuManager",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "McuManager",
+            targets: ["McuManager"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/unrelentingtech/SwiftCBOR.git", 
+            .branch("master")
+        ),
+    ],
+    targets: [
+        .target(name: "McuManager")
+    ]
+)
